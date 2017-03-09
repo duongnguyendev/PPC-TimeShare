8e528582b3fd119e94b3eb864bed9643a73ac8ed
Nguyen Duy Duong <duyduong9xx@gmail.com>
2017-03-09 09:37:06 +0700

    Update search

---
+++ PPC-TimeShare/PPC-TimeShare/Views/Cell/SearchResultCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResultCell.swift
+//  PPC-TimeShare
+//
+//  Created by Nguyen Duy Duong on 3/9/17.
+//  Copyright © 2017 Nguyen Duy Duong. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: BaseCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
